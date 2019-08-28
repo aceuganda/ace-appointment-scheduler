97f4c2c88b0258515b63cbf20411717adfd14298
King-Mike <nsubugamike021@gmail.com>
2019-08-28 09:49:31 +0300

    draft out functionality methods

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import injectTapEventPlugin from "react-tap-event-plugin";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// injectTapEventPlugin();
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      //init state
+      loading: true,
+      navOpen: false,
+      confirmationModalOpen: false,
+      confirmationTextVisible: false,
+      stepIndex: 0,
+      appointmentDateSelected: false,
+      appointmentMeridiem: 0,
+      validEmail: false,
+      validPhone: false,
+      smallScreen: window.innerWidth < 768,
+      confirmationSnackbarOpen: false
+    };
+    //method bindings
+    this.handleNavToggle = this.handleNavToggle.bind(this);
+    this.handleNextStep = this.handleNextStep.bind(this);
+    this.handleSetAppointmentDate = this.handleSetAppointmentDate.bind(this);
+    this.handleSetAppointmentSlot = this.handleSetAppointmentSlot.bind(this);
+    this.handleSetAppointmentMeridiem = this.handleSetAppointmentMeridiem.bind(
+      this
+    );
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
+    this.validatePhone = this.validatePhone.bind(this);
+    this.checkDisableDate = this.checkDisableDate.bind(this);
+    this.renderAppointmentTimes = this.renderAppointmentTimes.bind(this);
+    this.renderConfirmationString = this.renderConfirmationString.bind(this);
+    this.renderAppointmentConfirmation = this.renderAppointmentConfirmation.bind(
+      this
+    );
+    this.resize = this.resize.bind(this);
+  }
+  handleNavToggle() {}
+
+  handleNextStep() {}
+
+  handleSetAppointmentDate(date) {}
+
+  handleSetAppointmentSlot(slot) {}
+
+  handleSetAppointmentMeridiem(meridiem) {}
+
+  handleFetch(response) {}
+
+  handleFetchError(err) {}
+
+  handleSubmit() {}
+
+  validateEmail(email) {}
+
+  validatePhone(phoneNumber) {}
+
+  checkDisableDate(date) {}
+
+  renderConfirmationString() {}
+
+  renderAppointmentTimes() {}
+
+  renderAppointmentConfirmation() {}
+
+  resize() {}
+  componentWillMount() {}
+  componentWillUnmount() {}
+  render() {
+    return <div>Hello World</div>;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import "normalize.css";
+const app = (
+  <MuiThemeProvider>
+    <App></App>
+  </MuiThemeProvider>
+);
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
