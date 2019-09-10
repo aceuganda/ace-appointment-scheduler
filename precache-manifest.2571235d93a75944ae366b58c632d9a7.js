188292eb7f5d9cad1ba91233494e34cc048beeb4
King-Mike <nsubugamike021@gmail.com>
2019-09-10 09:32:40 +0300

    Updates

--- precache-manifest.2571235d93a75944ae366b58c632d9a7.js
+++ precache-manifest.2571235d93a75944ae366b58c632d9a7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9b1ae265cfb081c4e27e5d36562b8cb6",
+    "revision": "68f992f37a9efe3c9311af185b9d4f13",
     "url": "/ace-appointment-scheduler/index.html"
   },
   {
-    "revision": "f3d31d3979167534e87c",
+    "revision": "66d7f24694ad0020e3e3",
     "url": "/ace-appointment-scheduler/static/css/2.d5276c4c.chunk.css"
   },
   {
-    "revision": "f3d31d3979167534e87c",
-    "url": "/ace-appointment-scheduler/static/js/2.4080076a.chunk.js"
+    "revision": "66d7f24694ad0020e3e3",
+    "url": "/ace-appointment-scheduler/static/js/2.175a8f93.chunk.js"
   },
   {
-    "revision": "4cdbd031c71c3864bc82",
-    "url": "/ace-appointment-scheduler/static/js/main.a2aac0f8.chunk.js"
+    "revision": "94be2451a3b96474f3b0",
+    "url": "/ace-appointment-scheduler/static/js/main.56d5d32d.chunk.js"
   },
   {
     "revision": "2036d8c0ef0402c799c1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ace-appointment-scheduler/precache-manifest.679e41b5f413c33265c08b5ad7dc6536.js"
+  "/ace-appointment-scheduler/precache-manifest.2571235d93a75944ae366b58c632d9a7.js"
 );
 
 self.addEventListener('message', (event) => {
