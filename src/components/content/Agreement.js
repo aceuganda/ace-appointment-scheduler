3b13d4ae4e385e3a819a092b5f12407e9704c119
Chris <chris.m.clarke@live.co.uk>
2019-09-16 14:38:18 +0100

    refactor agreement, confirmation and example question

---
+++ src/components/content/Agreement.js
@@ -0,0 +1,93 @@
+import React from "react";
+
+export const Agreement = () => (
+  <section>
+    <p>
+      Request to use equipment, computers or facilities shall be made at least 3
+      working days prior through IDI ACE Bioinformatics authorities.
+    </p>{" "}
+    <p>
+      No person shall without authority access or attempt to gain access to any
+      computer and or facility.
+    </p>
+    <p>
+      The granted access will only be for a limited time only as requested for
+      by the user, but can be extended or reduced depending on the availability
+      of the facility all at the discretion of the Facilities manager.
+    </p>
+    <p>
+      Access shall only be to very specific information and will not include
+      access to information that the user does not require access to.
+    </p>
+    <p>
+      The ACE designee must be present and onsite when the facility or equipment
+      are being used.
+    </p>
+    <p>
+      IDI/designee will assume the responsibility of supervising the user to
+      ensure compliance and protecting the facilities.
+    </p>
+    <p>
+      Facility use and or use of computers shall be on first come first serve
+      basis, but manager/in-charge may give priority
+    </p>
+    <p>
+      While using the facility, cell phones and other gadgets should be set
+      silent or turned off to avoid disruptions and any applications with audio
+      output should be used with headphones.
+    </p>
+    <p>
+      Conversations and discussions should also be kept silent to avoid
+      disrupting other users
+    </p>
+    <p>
+      Users of the computer clusters should avoid saving their work on the
+      computers as these would be deleted. Files must be saved on personal hard
+      drives on USB/Flash drive after a day’s work to avoid loss.
+    </p>
+    <p>
+      Personal, sensitive and confidential data types shall not be stored on
+      external hosted service such as Dropbox or Google docs.
+    </p>
+    <p>
+      Request and approval must be sought from the ACE bioinformatics
+      head/manager and head of department to use the data.
+    </p>
+    <p>
+      Only the ACE bioinformatics IT systems manager will be permitted to store,
+      make manipulations and backup data. Outside users are discouraged from
+      attempting to make any changes to data and any such changes whether
+      intentional or accidental must be reported.
+    </p>
+    <p>
+      Accessing pornography or profane content and using the facility to
+      disseminate offensive, threatening or abusive messages is strictly
+      prohibited in this facility. Individuals who violate this prohibition will
+      be subject to disciplinary action that may result into loss of access or
+      ban.
+    </p>
+    <p>
+      Non facility staff and students are prohibited from changing any computer
+      hardware or software. This violation may lead to loss of facility
+      privileges.
+    </p>
+    <p>
+      No external user shall remove, interfere or tamper with any components of
+      the facility or computer or use any computer system in ways that interfere
+      with its use
+    </p>
+    <p>
+      Damage to computers or facilities will lead to fines and may in some cases
+      result into loss or denial of facility access privileges
+    </p>
+    <p>
+      A record shall be maintained to indicate arrival and departure of visitors
+      or persons utilising the facility
+    </p>
+    <p>
+      No food or drinks shall be consumed in the computer rooms. Any accidental
+      spills on equipment and computers must be reported after which an
+      assessment shall be done to determine damage.
+    </p>
+  </section>
+);
